7b5eaed232c7f8f38c28d7f1dde879d65276a688
RizkyDSusanto <dwisusantomuhammadrizky@gmail.com>
2024-06-20 17:10:49 +0700

    reverse final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 // ini javascript/js
 
 function konver(){
-    let inputan = document.getElementById("inputC").value;
-    let hasil = parseInt(inputan) * 1.8 + 32;
-    document.getElementById("inputF").value = hasil;
-    let perhitungan = inputan + "°C * 1.8 + 32 = " + hasil;
-    document.getElementById("hasilHitung").value = perhitungan;
+    if (document.getElementById("inputC").value == "") {
+        alert("Masukkan suhu Celcius masih kosong. Silakan isi dahulu.");
+    } else {
+        let inputan = document.getElementById("inputC").value;
+        let hasil = parseInt(inputan) * 1.8 + 32;
+        document.getElementById("inputF").value = hasil;
+        let perhitungan = "(" + inputan + " °C * 1.8) + 32 = " + hasil + " °F";
+        document.getElementById("hasilHitung").value = perhitungan;
+    }
 }
 
 function reset(){
     document.getElementById("inputC").value = "";
     document.getElementById("inputF").value = "";
     document.getElementById("hasilHitung").value = "";
+}
+
+function reverse(){
+    if (document.getElementById("inputF").value == "") {
+        alert("Masukkan suhu Fahrenheit masih kosong. Silakan isi dahulu.");
+    } else {
+        let inputan = document.getElementById("inputF").value;
+        let hasil = (parseInt(inputan) - 32) / 1.8;
+        document.getElementById("inputC").value = hasil;
+        let perhitungan = "(" + inputan + " °F - 32) / 1.8 = " + hasil + " °C";
+        document.getElementById("hasilHitung").value = perhitungan;
+    }
 }
